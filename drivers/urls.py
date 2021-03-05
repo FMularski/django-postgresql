e733734fcf28eae772470d2a0b18d3f731bed5ec
Filip Mularski <mularskif@gmail.com>
2021-03-05 21:51:12 +0100

    drivers index&detail view

--- drivers/urls.py
+++ drivers/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'drivers'
+
 urlpatterns = [
-    
+    path('', views.DriversIndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DriversDetailView.as_view(), name='detail')
 ]
--- drivers/views.py
+++ drivers/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import  generic
+from .models import Driver
 
-# Create your views here.
+
+class DriversIndexView(generic.ListView):
+    template_name = 'drivers/index.html'
+    context_object_name = 'drivers'
+
+    def get_queryset(self):
+        return Driver.objects.all()
+
+
+class DriversDetailView(generic.DetailView):
+    template_name = 'drivers/detail.html'
+    model = Driver
