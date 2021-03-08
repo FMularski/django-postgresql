e30a0b3cf5c272dcb87b32b03f731b443a9537d8
Filip Mularski <mularskif@gmail.com>
2021-03-08 20:01:38 +0100

    change forms to ModelForms

--- cars/forms.py
+++ cars/forms.py
@@ -1,9 +1,12 @@
-from django import forms
-from drivers.models import Driver
+from django.forms import ModelForm
+from .models import Car
 
-class CarForm(forms.Form):
-    brand = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
-    model = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
-    year = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'user-input'}))
-    vin = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
-    owner = forms.ModelChoiceField(queryset=Driver.objects.all() )
+class CarForm(ModelForm):
+    # brand = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
+    # model = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
+    # year = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'user-input'}))
+    # vin = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
+    # owner = forms.ModelChoiceField(queryset=Driver.objects.all() )
+    class Meta:
+        model = Car
+        fields = ['brand', 'model', 'year', 'vin', 'owner']
--- drivers/forms.py
+++ drivers/forms.py
@@ -1,5 +1,9 @@
-from django import forms
+from django.forms import ModelForm
+from .models import Driver
 
-class DriverForm(forms.Form):
-    name = forms.CharField(label='Name:', max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
-    license = forms.CharField(label='License:', max_length=64, widget=forms.TextInput(attrs={'class': 'user-input'}))
+class DriverForm(ModelForm):
+    # name = forms.CharField(label='Name:', max_length=255, widget=forms.TextInput(attrs={'class': 'user-input'}))
+    # license = forms.CharField(label='License:', max_length=64, widget=forms.TextInput(attrs={'class': 'user-input'}))
+    class Meta:
+        model = Driver
+        fields = ['name', 'license']
